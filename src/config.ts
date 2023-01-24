927eb5e9d5ee4aa4cda13bbde7fcb099bdc618f3
Vincent Nabet <1830490+vnabet@users.noreply.github.com>
2023-01-24 17:29:23 +0100

    Correction titre du site

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Mileos - Gestion des risques Mildiou';
+export const SITE_TITLE = 'Gestion des risques Mildiou';
 export const SITE_DESCRIPTION = 'Mileos - Gestion des risques Mildiou';
